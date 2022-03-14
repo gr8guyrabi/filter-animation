4703d279dec2831ef0e9c20f61821816f582f078
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-03-15 01:14:48 +0545

    add: framer-motion and fetch popular movies from imdb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import './App.css'
+
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    const [movies, setMovies] = useState([])
+    
+    const fetchPopularMovies = async () => {
+        const data = await fetch(`https://imdb-api.com/en/API/MostPopularMovies/${process.env.REACT_APP_IMDB_API_KEY}`)
+        const popularMovies = await data.json()
+        setMovies(popularMovies.items)
+    }
+
+    useEffect(() => {
+        fetchPopularMovies()
+    }, [])
+
+    return (
+        <div className="App">
+            <h1>
+                App
+            </h1>
+        </div>
+    )
 }
 
 export default App
